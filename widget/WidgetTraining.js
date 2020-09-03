0d10412a43dc82309e34caa49fd5841cb8082fd4
Vladimir <kartayev@gmail.com>
2020-09-03 13:47:30 +0300

    methods working

--- widget/WidgetTraining.js
+++ widget/WidgetTraining.js
@@ -1,13 +1,13 @@
 define([
-  'dojo/_base/declare',
-  'dojo/_base/fx',
-  'dojo/_base/lang',
-  'dojo/on',
-  'dojo/dom-style',
-  'dijit/_WidgetBase',
-  'dijit/_OnDijitClickMixin',
-  'dijit/_TemplatedMixin',
-  'dojo/text!./templates/template.html',
+  "dojo/_base/declare",
+  "dojo/_base/fx",
+  "dojo/_base/lang",
+  "dojo/on",
+  "dojo/dom-style",
+  "dijit/_WidgetBase",
+  "dijit/_OnDijitClickMixin",
+  "dijit/_TemplatedMixin",
+  "dojo/text!./templates/template.html",
 ], (
   declare,
   fx,
@@ -22,23 +22,24 @@ define([
   return declare([_WidgetBase, _OnDijitClickMixin, _TemplatedMixin], {
     templateString: template,
 
-    baseClass: 'page-overlay',
+    baseClass: "page-overlay",
 
-    title: 'Loading...',
+    title: "Loading...",
 
-    loader: require.toUrl('../loading.gif'),
+    loader: require.toUrl("../loading.gif"),
 
     postCreate: function () {
       this.titleNode.innerHTML = this.title;
       this.loaderNode.src = this.loader;
-
-      this.own(on(this.domNode, 'onclick', lang.hitch(this, 'endLoading')));
     },
 
-    endLoading: function () {
-      fx.fadeOut({
-        node: this.domNode,
-      }).play();
+    endLoading: function (loading) {
+      !loading &&
+        fx
+          .fadeOut({
+            node: this.domNode,
+          })
+          .play();
     },
   });
 });
